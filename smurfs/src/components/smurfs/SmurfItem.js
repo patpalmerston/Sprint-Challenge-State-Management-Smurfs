f24ff5d8d524e2d13e0d73e77b5b26850ec37be6
Pat Palmerston <patpalmerston@gmail.com>
2019-08-16 08:31:48 -0700

    component paths cleared and types made

---
+++ smurfs/src/components/smurfs/SmurfItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SmurfItem = () => {
+	return <div>smurf Item!</div>;
+};
+
+export default SmurfItem;
---
+++ smurfs/src/components/smurfs/SmurfList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SmurfItem from './SmurfItem';
+
+const SmurfList = () => {
+	return (
+		<div>
+      
+			<SmurfItem />
+		</div>
+	);
+};
+
+export default SmurfList;
